c5d25941d6e4335d24a9d5aa4469537b1368826f
Nimra Mursaleen <raonimra337@gmail.com>
2026-06-05 13:48:25 +0500

    update

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function NavBar() {
+  return (
+    <div>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="/img/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Navbar
